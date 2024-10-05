083407a8737f039e11c53ec78a8dd6f4dc6e3028
gasoaress <gabrielsoaresramos05@gmail.com>
2024-10-04 21:31:14 -0300

    Correto

--- Trabalho2/tokens.h
+++ Trabalho2/tokens.h
@@ -3,3 +3,15 @@
 #define INT 258
 #define GE 259
 #define IF 260
+#define LE 261
+#define NOT 262
+#define FOR 263
+#define FLOAT 264
+#define GR 265
+#define AND 266
+#define OR 267
+#define WHILE 268
+#define ELSE 269
+#define RT 270
+#define EQ 271
+#define STR 272
